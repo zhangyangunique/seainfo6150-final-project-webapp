c0b2a467305e162de9ce92c97b2f1c8ab5c13f7f
April Bingham <a.bingham@northeastern.edu>
2020-08-26 19:36:13 -0700

    moving treefrog source

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Form from "../Form/Form";
+import treefrog from "../images/treefrog.jpg";
 
 const Home = () => {
     return (
         <div>
             The home page
-            <img src="/images/treefrog.jpg" alt="tree frog" />
+            <img src={treefrog} alt="tree frog" />
             <Form />
         </div>
     )
