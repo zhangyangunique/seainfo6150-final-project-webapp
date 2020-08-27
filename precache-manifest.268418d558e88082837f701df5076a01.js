5cf4088a21e9a9b0358a95aeb89495a28b10bc02
April Bingham <a.bingham@northeastern.edu>
2020-08-26 17:12:04 -0700

    Updates

---
+++ precache-manifest.268418d558e88082837f701df5076a01.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e05596441c0634a45f7ea57907f2939",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "50be51225740ab735e77",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.a42a29ec.chunk.js"
+  },
+  {
+    "revision": "7fac52ec39a52578bfba",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.e768500f.chunk.js"
+  },
+  {
+    "revision": "940ec4a42937c37ae625",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime~main.c4d7f5e8.js"
+  }
+]);
