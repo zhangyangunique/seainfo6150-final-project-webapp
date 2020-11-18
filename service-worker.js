a40824c3efdf53b4f56756795a03d28432d915ea
April Bingham <a.bingham@northeastern.edu>
2020-11-17 20:00:41 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/seainfo6150-final-project-webapp/precache-manifest.e7c8d0c687995ec427bda817642e16d5.js"
+  "/seainfo6150-final-project-webapp/precache-manifest.e8114e590589584a107472aaf616bf5d.js"
 );
 
 self.addEventListener('message', (event) => {
